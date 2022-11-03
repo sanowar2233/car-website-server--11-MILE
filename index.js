72ced70f7739849032acd4a840893ae6daee9756
sanowar2233 <sanowarhossain1082@gmail.com>
2022-11-04 03:40:29 +0600

    add

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ async function run(){
 
         })
 
+        // DELETE----------------
+
+        app.delete('/orders/:id', async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)}
+            const result=await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
     }
     finally{
         
